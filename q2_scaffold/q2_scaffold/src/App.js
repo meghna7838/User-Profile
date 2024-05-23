bcef4ba750dcdbbc810b2f1e56b0aac3427495fb
meghna7838 <meghnatandon7838@gmail.com>
2024-05-23 23:31:02 +0530

    Made changes in code and added styling

--- q2_scaffold/q2_scaffold/src/App.js
+++ q2_scaffold/q2_scaffold/src/App.js
@@ -1,11 +1,17 @@
 import "./styles.css";
 import { Component } from "react";
 // Use Hero, Skills and About component to display your information
+import Hero from "./Hero";
+import Skills from "./Skills";
+import About from "./About";
+
 class App extends Component {
   render() {
     return (
       <>
-      {/* render your component here  */}
+      <Hero/>
+      <Skills/>
+      <About/>      
       </>
 
     )}
--- q2_scaffold/q2_scaffold/src/Hero.js
+++ q2_scaffold/q2_scaffold/src/Hero.js
@@ -1,3 +1,17 @@
 // Create component here to display the Basic information such as 
 // Name: Email: Phone: Address:
 // Make sure to include these in your code with semicolon
+import { Component } from "react";
+
+class Hero extends Component{
+    render(){
+        <>
+        <h2>Name: Pranav Sharad Yeole</h2>
+        <p>Email: pranav@google.com</p>
+        <p>Phone: 8546465544</p>
+        <p>Address: ABC,xyz street</p>
+        </>
+    }
+}
+
+export default Hero;
